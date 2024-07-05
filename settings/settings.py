510e22ac03243bc8b0f5c03f24cae3395106e448
NicatDev <nicat254memmedov@gmail.com>
2024-07-05 20:26:50 +0400

    22

--- settings/settings.py
+++ settings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9l_%ojsx%hw(eio&m_zg#aby_lazy7_*np$)%+=(vmm!z2#^%7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = True
 
 ALLOWED_HOSTS = ['*']
